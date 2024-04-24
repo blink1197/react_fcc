f71645d254e32d71ab4fa5205dca170e7bca7974
blink1197 <cgbagotsay11@gmail.com>
2024-04-24 19:58:10 +0800

    Final commit

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 // Header Component
 const Header = () => {
@@ -14,8 +14,21 @@ const Header = () => {
   );
 }
 
+const TextInputs = (props) => {
+  return (
+    <div className='text-inputs'>
+      <h2>{props.name === 'topText' ? "Top Text" : "Bottom Text"}</h2>
+      <input type="text" 
+        name={props.name} 
+        onChange={props.handleChange} 
+        value = {props.value} />
+    </div>
+);
+}
+
 // Main Component
 const Meme = () => {
+  
   // State to store meme details
   const [meme, setMeme] = useState({
     topText: '',
@@ -28,60 +41,60 @@ const Meme = () => {
   const [allMemesImages, setAllMemeImages] = useState ({});
   
   // Async function to fetch memes from the API
-  async function fetchMemes() {
-    const apiUrl = 'https://api.imgflip.com/get_memes';
-    try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      if (data.success) {
-        setAllMemeImages(data.data.memes); // Save the memes to state
+  useEffect(() => {
+    const fetchMemes = async () => {
+      const apiUrl = 'https://api.imgflip.com/get_memes';
+      try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.success) {
+          setAllMemeImages(data.data.memes); // Save the memes to state
+        }
+      } catch (error) {
+        console.error('Error fetching memes:', error);
       }
-    } catch (error) {
-      console.error('Error fetching memes:', error);
-    }
-  }
+    };
+
+    fetchMemes();
+  }, []);
 
   // Function to select a random meme and set the states for image url and alt text
   const selectMeme = () => {
     const randomIndex = Math.floor(Math.random() * allMemesImages.length);
     setMeme(prevState => ({
       ...prevState,
+      topText: '',
+      bottomText: '',
       randomImage: `${allMemesImages[randomIndex].url}`,
       altText: `${allMemesImages[randomIndex].name}`
     }))
   }
 
-  // Function to change the top text of the meme.
-  const changeTopText = (event) => {
-    setMeme(prevState => ({
-      ...prevState,
-      topText: `${event.target.value}`
-    }))
+ 
+  // Function to change the top or bottom text of the meme.
+  const changeText = (event) => {
+    if (!meme.randomImage) {
+      alert("Please generate new meme image first.");
+      return;
+    }
+
+    const { name, value } = event.target;
+    if (name === 'bottomText' || name === 'topText') {
+      setMeme(prevState => ({
+        ...prevState,
+        [name]: value
+      }));
+    }
+    
   }
 
-  // Function to change the bottom text of the meme.
-  const changeBottomText = (event) => {
-    setMeme(prevState => ({
-      ...prevState,
-      bottomText: `${event.target.value}`
-    }))
-  }
-
-  //Fetch memes and save the memes into the state.
-  fetchMemes();
 
   return (
     <main>
       <form className='form'>
         <div className="input-container">
-          <div className='text-inputs'>
-            <h2>Top Text</h2>
-            <input type="text" onChange={changeTopText} />
-          </div>
-          <div className='text-inputs'>
-            <h2>Bottom Text</h2>
-            <input type="text" onChange={changeBottomText} />
-          </div>
+          <TextInputs name="topText" handleChange={changeText} value={meme.topText}/>
+          <TextInputs name="bottomText" handleChange={changeText} value={meme.bottomText}/>
         </div>
         <div className='GenerateButton' >
           <button type="button" onClick={selectMeme}>Get a new meme image  🖼️</button>
